df9be4642e8b0c18036f59c495fa6c046b66e359
Alexander Härle <alexander.haerle@gmx.net>
2022-11-09 21:21:47 +0100

    change model creation

--- trainKNN.py
+++ trainKNN.py
@@ -19,7 +19,8 @@ class Model:
             with open(f'models/{self.name}', 'w') as j:
                 pass
 
-            self.model: dict = {"200": [], "100": [], "50": [], "20": [], "10": [], "5": [], "2": [], "1": []}
+            self.model: dict = {}
+            
             self.write_model()
 
 
@@ -33,8 +34,8 @@ class Model:
     def update_small_model(self, coin: str, wight_update):
         """Update one coin wight for the small model"""
 
-        # if the model has no enty 
-        if len(self.model[coin]) == 0:
+        # if the model has no entry 
+        if self.model.get(coin) == None:
             print(f"Add new entry for {coin} coins at model {self.name}")
             wight_update += (1,)
             self.model[coin] = (wight_update)
@@ -56,40 +57,50 @@ class Model:
 
     def update_large_model(self, coin: str, wight_update):
         """Update one coin wight for the large model"""
-        if len(self.model[coin]) == 0:
+        if self.model.get(coin) == None:
             print(f"Add new entry for {coin} coins at model {self.name}")
+            self.model[coin] = [(wight_update)]
+            self.write_model()
+            return
 
         self.model[coin].append(list(wight_update))
         self.write_model()
         
 
 if __name__ == "__main__":
+    # old working models
     # model_small = Model("model_1.json", model_type="small")
     # model_large = Model("model_2.json", model_type="large")
 
-    model_small = Model("model_small.json", model_type="small")
-    model_large = Model("model_large.json", model_type="large")
+    # test models
+    # model_small = Model("model_small.json", model_type="small")
+    # model_large = Model("model_large.json", model_type="large")
 
-    model_small.update_small_model("50", (202, 155, 645, 170))
-    model_large.update_large_model("100", (202, 155, 645, 170))
+    # current models
+    model_small = Model("small.json", model_type="small")
+    model_large = Model("large.json", model_type="large")
+
+    # model_small.update_small_model("200", (202, 155, 645, 170))
+    # model_large.update_large_model("200", (202, 155, 645, 170))
 
 
 
-    # messwerte = [210, 838, 985, 340, 864, 130, 719, 255, 782, 992, 732, 497, 811, 623, 172, 700, 283, 951, 504, 770, 516, 51, 900, 806, 197, 485, 1000, 987, 573, 6, 758, 653, 386, 423, 398, 649, 34, 184, 519, 901, 952, 447, 319, 199, 714, 302, 235, 161, 767, 958]
 
-    # x = prepData(messwerte)
+    messwerte = [210, 838, 985, 340, 864, 130, 719, 255, 782, 992, 732, 497, 811, 623, 172, 700, 283, 951, 504, 770, 516, 51, 900, 806, 197, 485, 1000, 987, 573, 6, 758, 653, 386, 423, 398, 649, 34, 184, 519, 901, 952, 447, 319, 199, 714, 302, 235, 161, 767, 958]
 
-    # np_model_small = np.array(list(model_small.model.values()))
-    # np_model_large = np.array(list(model_large.model.values()))
+    x = prepData(messwerte)
 
-    # if model_small.model_type == "small": np_model_small = np.delete(np_model_small, 4, 1) # remove the amount column which is only required to train the small model
-    # if model_large.model_type == "small": np_model_large = np.delete(np_model_large, 4, 1) # remove the amount column which is only required to train the small model
+    np_model_small = np.array(list(model_small.model.values()))
+    np_model_large = np.array(list(model_large.model.values()))
 
-    # idx_knn_small = getKNearestNeighbors(x, np_model_small, 1)
-    # idx_knn_large = getKNearestNeighbors(x, np_model_large, 1)
+    if model_small.model_type == "small": np_model_small = np.delete(np_model_small, 4, 1) # remove the amount column which is only required to train the small model
+    if model_large.model_type == "small": np_model_large = np.delete(np_model_large, 4, 1) # remove the amount column which is only required to train the small model
 
-    # print(f'Small model index: {idx_knn_small}')
-    # print(f'Large model index: {idx_knn_large} \n')
+    idx_knn_small = getKNearestNeighbors(x, np_model_small, 1)
+    idx_knn_large = getKNearestNeighbors(x, np_model_large, 1)
 
-    # print(f'Small model prediction: {model_small.keyMapping[idx_knn_small[0]]}')
-    # print(f'Large model prediction: {model_large.keyMapping[idx_knn_large[0]]}')
+    print(f'Small model index: {idx_knn_small}')
+    print(f'Large model index: {idx_knn_large} \n')
+
+    print(f'Small model prediction: {model_small.keyMapping[idx_knn_small[0]]}')
+    print(f'Large model prediction: {model_large.keyMapping[idx_knn_large[0]]}')

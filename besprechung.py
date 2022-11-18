a7680955b162506cd79745853c92886eef5128d1
Alexander Härle <alexander.haerle@gmx.net>
2022-11-18 15:00:17 +0100

    besprechung

---
+++ besprechung.py
@@ -0,0 +1,18 @@
+# modell
+data_database = {"200": [[123,123,123,123], [123,123,123,123],[123,123,123,123],[123,123,123,123]], "100":[[230,195,394,143]]}
+
+# modell_database (gemittelte modell)
+
+def get_all():
+    """ to create the compact model
+    """
+    pass
+
+def get_Model_labels():
+    return ["2€", "..."]
+
+def get_model_data():
+    pass
+
+def evaluate():
+    pass
